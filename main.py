54fe6ad8c1a61aa30d3780671027f335a57f07d8
Federico <as>
2021-05-16 20:04:55 +0100

    autoexport - temperature - confirm export

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # imports
 import tkinter as tk
 import pandas as pd
+from tkinter.messagebox import showinfo
 from winsound import Beep
 from time import perf_counter, strftime, gmtime
 from datetime import datetime
@@ -10,6 +11,8 @@ from os import path
 
 # general settings
 up_time = 1_000  # ms
+temperature = 0
+# save_path = 'C:/Users/mc9241x/Desktop/Timer Data'
 save_path = 'C:/Users/Federico/Desktop/test'
 
 # GUI style
@@ -26,6 +29,10 @@ c3 = '#dfe6e9'
 def sound(n):
     for _ in range(n):
         Beep(1500, 200)
+
+
+def confirm_sound():
+    Beep(2200, 200)
 
 
 def format_n(n):
@@ -67,17 +74,22 @@ def export_data():
     current_t = datetime.now().strftime('%Y-%m-%d_%H-%M')
     
     # get counters info
-    for c in [info['probe'], info['sensing']]:
-        for t in c.timestamps:
-            c_info = {'event': c.txt,
-                      'start time': t,
-                      'start time (m)': format_m(t)}
-            data = data.append([c_info])
+    for c in info.values():
+        if isinstance(c, Counter):
+            for t in c.timestamps:
+                c_info = {'event': c.txt,
+                          'start time': t,
+                          'start time (m)': format_m(t)}
+                data = data.append([c_info])
     
     # export to excel
-    file_name = f'test_{current_t}.xlsx'
+    file_name = f'timer_{current_t}_{temperature}.xlsx'
     dest = path.join(save_path, file_name)
     data.to_excel(dest, index=False)
+    
+    # confirmation messagebox
+    showinfo('Confirm Save', f'Experiment exported in {save_path}')
+    reset_data()
 
 
 def reset_data():
@@ -101,6 +113,13 @@ def remove_last():
 def log_update():
     info['log'].txt = '\n'.join([f'{a} - {b}' for a, b in zip(data['event'].values, data['duration (s)'].values)])
     info['log'].lbl.configure(text=info['log'].txt)
+
+
+def end_main_reset():
+    for i in info.values():
+        if isinstance(i, Timer):
+            if not i.stop:
+                i.stop_t()
 
 
 # GUI elements
@@ -213,6 +232,24 @@ class Btn:
         self.btn.grid(padx=0, pady=0)
 
 
+class Radio:
+    def __init__(self, root, val, row, col):
+        # get info
+        self.root = root
+        self.val = val
+        self.row = row
+        self.col = col
+        
+        # create radio button
+        self.rad = tk.Radiobutton(self.root, text=self.val, variable=1, value=self.val, font=(font, size // 2))
+        self.rad.configure(bg=c2, fg=c0, activebackground=c2, activeforeground=c0, command=self.sel)
+        self.rad.grid(row=self.row, column=self.col, padx=pad, pady=pad)
+    
+    def sel(self):
+        global temperature
+        temperature = self.val
+
+
 # timer elements
 class Timer:
     """timestamp label with start/stop buttons and auto-update"""
@@ -248,6 +285,7 @@ class Timer:
             self.root.after(up_time, self.update_t)
     
     def start_t(self):
+        confirm_sound()
         self.stop = False
         self.t = perf_counter() - self.t
         self.update_t()
@@ -256,13 +294,13 @@ class Timer:
         self.start_cond()
     
     def stop_t(self):
+        confirm_sound()
         t = perf_counter() - self.t
         update_info(t, self.txt)
         self.reset_t()
         self.stop_cond()
     
     def reset_t(self):
-        # reset timer
         self.stop = True
         self.t = 0
         self.timestamp.lbl.configure(text='00:00')
@@ -276,8 +314,8 @@ class Timer:
     def update_info(self):
         if self.txt == 'Landing':
             info['landing'] = self
-        if self.txt == 'Walking':
-            info['walking'] = self
+        if self.txt == 'Still':
+            info['still'] = self
         if self.txt == 'Feeding':
             info['feeding'] = self
         if self.txt == 'Probe & Sensing':
@@ -285,7 +323,7 @@ class Timer:
     
     def start_cond(self):
         if self.txt == 'Feeding':
-            for i in [info['walking'], info['probe_sensing']]:
+            for i in [info['still'], info['probe_sensing']]:
                 if not i.stop:
                     i.stop_t()
         if self.txt == 'Probe & Sensing':
@@ -314,30 +352,36 @@ class Countdown:
         self.update_info()
         
         # frame
-        fr = LblFrame(self.root, self.txt, self.row, self.col).lblframe
+        self.fr = LblFrame(self.root, self.txt, self.row, self.col).lblframe
         self.root.columnconfigure(self.col, weight=1)
         for i in range(2):
-            fr.rowconfigure(i, weight=1)
-            fr.columnconfigure(i, weight=1)
+            self.fr.rowconfigure(i, weight=1)
+            self.fr.columnconfigure(i, weight=1)
         
         # timestamp label
         stamp = strftime('%M:%S', gmtime(self.tot))
-        self.timestamp = Lbl(fr, stamp, 0, 0)
+        self.timestamp = Lbl(self.fr, stamp, 0, 0)
         self.timestamp.timer()
         if self.tot != 120:
             self.timestamp.lbl.configure(font=(font, size * 4))
         
         # buttons
-        self.start_pause = Btn(fr, 'Start', self.start_t, 1, 0)
+        self.start_pause = Btn(self.fr, 'Start', self.start_t, 1, 0)
         self.start_pause.timer()
-        self.reset = Btn(fr, 'Reset', self.reset_t, 1, 1)
+        self.reset = Btn(self.fr, 'Reset', self.reset_t, 1, 1)
         self.reset.timer()
     
     def update_t(self):
-        # beeps at the end and half time
+        # end of countdown
         if self.tot - (perf_counter() - self.t) <= 0:
             sound(4)
+            if self.tot != 120:
+                end_main_reset()
+                export_data()
+            self.reset_t()
             return
+        
+        # beep at half time
         if self.tot != 120 and self.tot // 2 - 1 <= self.tot - (perf_counter() - self.t) <= self.tot // 2 + 1:
             sound(1)
         
@@ -348,12 +392,14 @@ class Countdown:
             self.root.after(up_time, self.update_t)
     
     def start_t(self):
+        confirm_sound()
         self.pause = False
         self.t = perf_counter() - self.t
         self.update_t()
         self.start_pause.btn.configure(text='Pause', command=self.pause_t)
     
     def pause_t(self):
+        confirm_sound()
         self.pause = True
         self.t = perf_counter() - self.t
         self.start_pause.btn.configure(text='Start', command=self.start_t)
@@ -374,6 +420,9 @@ class Countdown:
             info['setup'] = self
         if self.txt == 'Observation':
             info['main'] = self
+    
+    def main_time(self):
+        self.fr.grid(rowspan=2)
 
 
 class Counter:
@@ -403,6 +452,7 @@ class Counter:
         Btn(fr, '-', lambda: self.change_val('-'), 0, 2).counter_small()
     
     def change_val(self, op):
+        confirm_sound()
         if op == '+':
             self.val += 1
             t = perf_counter() - info['main'].t
@@ -446,13 +496,20 @@ class App:
         
         # main and setup countdown
         main_fr = Frame(self.root, 0, 0).frame
-        Countdown(main_fr, 'Setup', 60 * 2, 0, 0).setup_time()
-        Countdown(main_fr, 'Observation', 60 * 10, 0, 1)
+        Countdown(main_fr, 'Setup', 60 * 2, 1, 0).setup_time()
+        Countdown(main_fr, 'Observation', 60 * 10, 0, 1).main_time()
         
-        # setup time, landing and walking
+        # temperature
+        temp_frame = LblFrame(main_fr, 'Temperature', 0, 0).lblframe
+        Radio(temp_frame, 30, 0, 0)
+        Radio(temp_frame, 36, 0, 1)
+        Radio(temp_frame, 42, 0, 2)
+        Radio(temp_frame, 48, 0, 3)
+        
+        # setup time, landing and still
         land_frame = Frame(self.root, 1, 0).frame
         Timer(land_frame, 'Landing', 0, 0).bind_key('<space>')
-        Timer(land_frame, 'Walking', 0, 1).bind_key('w')
+        Timer(land_frame, 'Still', 0, 1).bind_key('w')
         Timer(land_frame, 'Feeding', 0, 2).bind_key('n')
         
         # probe and sensing
